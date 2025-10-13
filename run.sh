e075f7cd8772be0c63c8847fa75c08fc9f3ef547
i18n <i18n.site@gmail.com>
2025-10-13 14:57:18 +0800

    ♨

--- run.sh
+++ run.sh
@@ -4,4 +4,8 @@ DIR=$(realpath $0) && DIR=${DIR%/*}
 cd $DIR
 set -ex
 
+if ! [ -x "$(command -v systemfd)" ]; then
+  cargo install systemfd
+fi
+
 systemfd -s tcp::0.0.0.0:8080 -s tcp::0.0.0.0:9083 -s udp::0.0.0.0:9083 -- cargo run -F cert_dir --example server
